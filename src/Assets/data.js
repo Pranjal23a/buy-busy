0825888f94b5838ca0cebc3ddaed7455ceb73994
Pranjal Sharma <sharmapranjal23a@gmail.com>
2023-11-06 23:18:26 +0530

    App completed

---
+++ src/Assets/data.js
@@ -0,0 +1,107 @@
+// data of all the product's in home page
+// id, name, image url , price and category of the product
+
+
+export const data = [
+    {
+        id: "1",
+        name: "Men's T-Shirt",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNN5PFMwW9Crgrxf_mhb1DbduaQcStJSSjhA&usqp=CAU",
+        price: 299,
+        category: "men"
+
+    },
+    {
+        id: "2",
+        name: "Men's Printed Casual Shirt",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLXA2WEnZgNcQKfsQVJ65ehhwS2DQpt5XsFsbuso7Pd8J5szwj4vGn6dAoGi1SO3HnVLY&usqp=CAU",
+        price: 349,
+        category: "men"
+
+    },
+    {
+        id: "3",
+        name: "Men's Slim Fit Black Stretch Jeans",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCHmVocGkyvAySrqeQ_gI_suEeKA4VjIb1_w&usqp=CAU",
+        price: 899,
+        category: "men"
+    },
+    {
+        id: "4",
+        name: "boAt Rockerz450 Bluetooth On Ear Headphones with Mic",
+        image: "https://m.media-amazon.com/images/I/51xxA+6E+xL._SX522_.jpg",
+        price: 1399,
+        category: "electric"
+    },
+    {
+        id: "5",
+        name: "Cuban Silver Bracelet for Men Boys",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbzhdZVexVRBAbmkgIPxrehGGcHwmfpBxayg&usqp=CAU",
+        price: 2199,
+        category: "jewellery"
+    },
+    {
+        id: "6",
+        name: "Laptop Backpack with USB Charging Port | Multi functional Bagpack (Grey)",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpEANWltIUZGQo5J6ec7MKmPRtayP7JmI_OA&usqp=CAU",
+        price: 1599,
+        category: "men"
+    },
+    {
+        id: "7",
+        name: "Noise Twist Bluetooth Calling Smart Watch ",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIaL4kHw8cutrOo0ZzVa_N-qcD1Pgx5Z4nRg&usqp=CAU",
+        price: 2499,
+        category: "electric"
+    },
+    {
+        id: "8",
+        name: "HP All-in-One Desktop PC (8GB/256GB SSD + 1TB HDD/FHD",
+        image: "https://m.media-amazon.com/images/I/61ogPth2lzL._SX679_.jpg",
+        price: 45999,
+        category: "electric"
+    },
+    {
+        id: "9",
+        name: "Women's Hoodies",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGJ5BguUC71ITs4vf-mRAe6_qne5WR2cR4IoTJ01vqaRS9hqXbconAmOlpJ3V5yfJUjhY&usqp=CAU",
+        price: 2799,
+        category: "women"
+    },
+    {
+        id: "10",
+        name: " Women's Sweatshirt 'M' size Pink ",
+        image: "https://images-cdn.ubuy.co.in/635b9d950075511c41796374-mmf-women-s-plus-sweatshirts-and.jpg",
+        price: 3499,
+        category: "women"
+    },
+    {
+        id: "11",
+        name: "Gold Jewellery Set necklace and earrings",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4P9ElNHL1X8KlCNJhwG6g904m5VmB9evbAw&usqp=CAU",
+        price: 25499,
+        category: "jewellery"
+    },
+    {
+        id: "12",
+        name: "Women Leather Bag",
+        image: "https://m.media-amazon.com/images/I/71Y32csE9vL._UY695_.jpg",
+        price: 4449,
+        category: "women"
+    },
+    {
+        id: "13",
+        name: "WildHorn Wallet for Men",
+        image: "https://m.media-amazon.com/images/I/914s7a8twcL._SX679_.jpg",
+        price: 649,
+        category: "men"
+    },
+    {
+        id: "14",
+        name: "HP Wired Keyboard",
+        image: "https://m.media-amazon.com/images/I/31-AngJwVkL._SX300_SY300_QL70_FMwebp_.jpg",
+        price: 599,
+        category: "electric"
+    }
+
+];
